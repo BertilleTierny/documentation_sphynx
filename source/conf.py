c6a79a3ac06be15c3765ebc8b14e48dbdbdb42c7
Bertille Tierny <bertille.tierny@milliman.com>
2026-01-06 10:24:28 +0100

    test

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,31 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
+    "sphinx_multiversion",
 ]
 
 templates_path = ['_templates']
+
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # Autres options...
+}
+
+# Si vous voulez forcer l'inclusion du template versions
+html_context = {
+    'display_github': True,
+    'github_user': 'BertilleTierny',
+    'github_repo': 'documentation_sphynx',
+    'github_version': 'main',
+    'conf_py_path': '/source/',
+}
+
+html_theme = 'sphinx_rtd_theme'
+
+
 exclude_patterns = []
 
 language = 'fr'
